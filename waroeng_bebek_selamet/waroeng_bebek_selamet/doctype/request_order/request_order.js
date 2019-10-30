e92572a9515c6350a61c948fe336415ae043af07
rockydc <rockydaniel182@gmail.com>
2019-10-30 12:45:21 +0700

    test push

--- waroeng_bebek_selamet/waroeng_bebek_selamet/doctype/request_order/request_order.js
+++ waroeng_bebek_selamet/waroeng_bebek_selamet/doctype/request_order/request_order.js
@@ -2,7 +2,19 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Request Order', {
-	refresh: function(frm) {
+	// refresh: function(frm) {
 
+	// }
+	id_menu: function(frm){
+		frm.doc.pesanan = []
+
+		if(frm.doc.id_menu){
+			frappe.call({
+				method: "frappe.client.get",
+				args: {
+					doctype: ""
+				}
+			,})
+		}
 	}
 });
